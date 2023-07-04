a7d24fa7c878ee00ab5ff52da4df38fce2286332
Kevin Kaminski MVP <Kevin.Kaminski@bighatgroup.com>
2023-07-03 19:57:38 -0600

    Not fully tested yet, but works when everything is installed.  Need to test on a clean system.

--- Install-WinGet.ps1
+++ Install-WinGet.ps1
@@ -1,20 +1,25 @@
+# Begin recording all interactions to the log file
+Start-Transcript -Path "$LogPath\wingetinstall.log"
+
 $progressPreference = 'silentlyContinue'
 
 # Set the environment variable for the root of the path
-$LogPath = $env:TEMP
- 
-# Begin recording all interactions to the log file
-Start-Transcript -Path "$LogPath\wingetinstall.log"
+$LogPath = $env:temp
+
+# Set the XML Dependency
+$webview2 = "Microsoft.Web.WebView2"
+$uixamldependency = "Microsoft.UI.Xaml"
+
+# Put in the VC library details
+$vcdependency = "Microsoft.VCLibs.x64.14.00.Desktop.appx"
+$vcdependencyname = "Microsoft.VCLibs.140.00"
 
 $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
 
-Write-Information "Downloading winget to artifacts directory..."
-
+Write-Information "Downloading winget to current directory..."
 Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 
 # Workaround for Microsoft.UI.Xaml dependency.
 # Creates an additional dependency on Microsoft.Web.Webview2
@@ -37,9 +42,32 @@ if(-not (Get-PackageSource | Where-Object { $_.Name -eq 'nuget.org' })) {
     Register-PackageSource -Name nuget.org -Location https://www.nuget.org/api/v2 -ProviderName NuGet
 }
 
-# Installs Microsoft.UI.Xaml and Microsoft.Web.Webview2 in one command
-Install-Package Microsoft.UI.Xaml -Force
-Install-AppxPackage -path "./$latestWingetMsixBundle" -force
+# Installs Microsoft.Web.Webview2
+# Check if UI Xaml is installed
+if (-not (Get-Package -Name $webview2 -ErrorAction SilentlyContinue)) {
+    Write-Information "Installing Microsoft UI XAML..."
+    Install-Package $webview2 -Force
+}
+
+# Installs Microsoft.UI.Xaml
+# Check if UI Xaml is installed
+if (-not (Get-Package -Name $UIXAMLDependency -ErrorAction SilentlyContinue)) {
+    Write-Information "Installing Microsoft UI XAML..."
+    Install-Package $UIXAMLDependency -Force
+}
+
+# Check if VC++ Libs  is installed
+if (-not (Get-AppxPackage -Name $vcdependencyname -ErrorAction SilentlyContinue)) {
+    Write-Information "Installing winget..."
+    Add-AppxPackage "./$VCDependency"
+}
+
+# Check if winget is installed
+$wingetPackageName = $latestWingetMsixBundle.Split(".")[0]
+if (-not (Get-AppxPackage -Name $wingetPackageName -ErrorAction SilentlyContinue)) {
+    Write-Information "Installing winget..."
+    Add-AppxPackage "./$latestWingetMsixBundle"
+}
 
 # Stop recording interactions
 Stop-Transcript
