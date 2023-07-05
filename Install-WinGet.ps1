3281f394e70445cd8786451d6f8dc1220771792f
Kevin Kaminski MVP <Kevin.Kaminski@bighatgroup.com>
2023-07-04 18:41:35 -0600

    Still prompts to install nuget but otherwise works fine.

--- Install-WinGet.ps1
+++ Install-WinGet.ps1
@@ -1,18 +1,15 @@
-# Begin recording all interactions to the log file
-Start-Transcript -Path "$LogPath\wingetinstall.log"
-
 $progressPreference = 'silentlyContinue'
-
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 # Set the environment variable for the root of the path
-$LogPath = $env:temp
+$LogPath = $env:TEMP
 
 # Set the XML Dependency
-$webview2 = "Microsoft.Web.WebView2"
-$uixamldependency = "Microsoft.UI.Xaml"
+$UIXAMLDependency = "Microsoft.UI.Xaml"
+$UIXAMLAPPXDependency = $env:ProgramFiles + "\PackageManagement\NuGet\Packages\Microsoft.UI.Xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
+$VCDependency = "Microsoft.VCLibs.x64.14.00.Desktop.appx"
 
-# Put in the VC library details
-$vcdependency = "Microsoft.VCLibs.x64.14.00.Desktop.appx"
-$vcdependencyname = "Microsoft.VCLibs.140.00"
+# Begin recording all interactions to the log file
+Start-Transcript -Path "$LogPath\wingetinstall.log"
 
 $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
@@ -23,11 +20,12 @@ Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -O
 
 # Workaround for Microsoft.UI.Xaml dependency.
 # Creates an additional dependency on Microsoft.Web.Webview2
-
 # Check if Nuget Package Provider is installed
-if(-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
-    # Install the Nuget Package Provider
-    Install-PackageProvider -Name NuGet -Force
+if(-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) 
+
+{
+# Install the Nuget Package Provider current user to be silent
+    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -MinimumVersion 2.8
 }
 
 # Check if Nuget Module is installed
@@ -37,34 +35,32 @@ if(-not (Get-Module -ListAvailable -Name NuGet)) {
 }
 
 # Check if Nuget.org repository is registered
-if(-not (Get-PackageSource | Where-Object { $_.Name -eq 'nuget.org' })) {
+if(-not (Get-PackageSource | Where-Object { $_.Name -eq 'nuget.org' })) 
+{
     # Register the Nuget.org repository
     Register-PackageSource -Name nuget.org -Location https://www.nuget.org/api/v2 -ProviderName NuGet
 }
 
-# Installs Microsoft.Web.Webview2
-# Check if UI Xaml is installed
-if (-not (Get-Package -Name $webview2 -ErrorAction SilentlyContinue)) {
-    Write-Information "Installing Microsoft UI XAML..."
-    Install-Package $webview2 -Force
-}
-
 # Installs Microsoft.UI.Xaml
 # Check if UI Xaml is installed
-if (-not (Get-Package -Name $UIXAMLDependency -ErrorAction SilentlyContinue)) {
+if (-not (Get-Package -Name $UIXAMLDependency -RequiredVersion 2.7 -ErrorAction SilentlyContinue)) 
+{
     Write-Information "Installing Microsoft UI XAML..."
-    Install-Package $UIXAMLDependency -Force
+    Install-Package $UIXAMLDependency -RequiredVersion 2.7 -Force
+    Add-AppxPackage $UIXAMLAPPXDependency
 }
 
 # Check if VC++ Libs  is installed
-if (-not (Get-AppxPackage -Name $vcdependencyname -ErrorAction SilentlyContinue)) {
-    Write-Information "Installing winget..."
-    Add-AppxPackage "./$VCDependency"
+if (-not (Get-AppxPackage -Name $VCDependency -ErrorAction SilentlyContinue)) 
+{
+    Write-Information "Installing VC dependency..."
+    Add-AppxPackage "./$VCDependency" -ErrorAction SilentlyContinue
 }
 
 # Check if winget is installed
 $wingetPackageName = $latestWingetMsixBundle.Split(".")[0]
-if (-not (Get-AppxPackage -Name $wingetPackageName -ErrorAction SilentlyContinue)) {
+if (-not (Get-AppxPackage -Name $wingetPackageName -ErrorAction SilentlyContinue)) 
+{
     Write-Information "Installing winget..."
     Add-AppxPackage "./$latestWingetMsixBundle"
 }
